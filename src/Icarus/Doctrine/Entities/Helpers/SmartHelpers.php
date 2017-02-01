444f4e7e6b647ede91a98d7cd9dbff4544fb39c5
Pavel Gajdoš <info@pavelgajdos.cz>
2017-02-01 12:39:07 +0100

    Added a universal helper trait with all available helpers

---
+++ src/Icarus/Doctrine/Entities/Helpers/SmartHelpers.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pavelgajdos
+ * Date: 01.02.17
+ * Time: 12:37
+ */
+
+namespace Icarus\Doctrine\Entities\Helpers;
+
+
+trait SmartHelpers
+{
+    use AssignmentHelper;
+    use EnumHelper;
+}
