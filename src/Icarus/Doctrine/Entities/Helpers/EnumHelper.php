ef26b4805383dc0a1fe12cf4ab8f1a811c93e671
Pavel Gajdoš <info@pavelgajdos.cz>
2017-02-01 10:37:10 +0100

    Added EnumHelper trait for checking values that are being set to columns with enum-like behaviour.

---
+++ src/Icarus/Doctrine/Entities/Helpers/EnumHelper.php
@@ -0,0 +1,31 @@
+<?php
+
+
+namespace Icarus\Doctrine\Entities\Helpers;
+
+
+use Kdyby\Doctrine\InvalidArgumentException;
+use Nette\Utils\Strings;
+
+
+trait EnumHelper
+{
+
+    private function checkEnumValue($constantsPrefix, $enumValue)
+    {
+        $allowedValues = [];
+        if (!$constantsPrefix) {
+            $allowedValues = $this->reflection->getConstants();
+        } else {
+            foreach ($this->reflection->getConstants() as $name => $value) {
+                if (Strings::startsWith($name, $constantsPrefix)) {
+                    $allowedValues[$name] = $value;
+                }
+            }
+        }
+
+        if (!in_array($enumValue, $allowedValues)) {
+            throw new InvalidArgumentException("Invalid value '$enumValue'. Must be one of the values of constants prefixed with '$constantsPrefix'.");
+        }
+    }
+}
