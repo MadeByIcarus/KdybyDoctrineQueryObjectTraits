08c1f257fad68576631d6f130380ace041ba077a
Pavel Gajdoš <info@pavelgajdos.cz>
2017-03-08 15:46:53 +0100

    Added getValues method

--- src/Icarus/Doctrine/Entities/Helpers/AssignmentHelper.php
+++ src/Icarus/Doctrine/Entities/Helpers/AssignmentHelper.php
@@ -15,6 +15,7 @@ use Nette\Utils\Strings;
 
 trait AssignmentHelper
 {
+
     public function assignValues(array $values)
     {
         foreach ($values as $key => $value) {
@@ -22,13 +23,46 @@ trait AssignmentHelper
 
             if ($this->reflection->hasMethod($method)) {
                 $this->{$method}($value);
-            }
-            else if ($this->reflection->hasProperty($key)) {
+            } else if ($this->reflection->hasProperty($key)) {
                 $this->$key = $value;
-            }
-            else {
+            } else {
                 throw new InvalidArgumentException("Entity does not have property '$key' or method '$method'.");
             }
         }
+    }
+
+
+
+    public function getValues($whitelist = null, $blacklist = ['id'])
+    {
+        $list = [];
+
+        foreach ($this->reflection->getProperties() as $property) {
+            if ($whitelist) {
+                if (in_array($property->getName(), $whitelist)) {
+                    $list[] = $property->getName();
+                } else {
+                    continue;
+                }
+            }
+
+            if ($blacklist) {
+                if (!in_array($property->getName(), $blacklist)) {
+                    $list[] = $property->getName();
+                } else {
+                    continue;
+                }
+            }
+
+            $list[] = $property->getName();
+        }
+
+        $values = [];
+
+        foreach ($list as $item) {
+            $values[$item] = $this->{$item};
+        }
+
+        return $values;
     }
 }
